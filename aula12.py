a7ac0ee8b38891c0954dfb375193c709424fd291
Felipe Oliveira <felipemtsg@gmail.com>
2024-10-24 16:42:41 -0300

    pasta de exercicios de python

---
+++ aula12.py
@@ -0,0 +1,10 @@
+# exercício 2
+# fazer calculadora de imc com meus dados
+# Felipe Oliveira tem 1.73 de altura, pesa 75 kg e seu IMC é ...
+
+nome = 'Felipe Oliveira'
+altura = 1.73
+peso = 75
+imc = peso / altura **2
+print(nome, str('tem'), altura, str('de altura,'))
+print('pesa', peso, 'kg e seu IMC é', imc)
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Felipe'
+sobrenome = 'Oliveira'
+idade = int(25)
+ano_nascimento = int(1999)
+maior_de_idade = idade >= 18
+altura_metros = float(1.72)
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
